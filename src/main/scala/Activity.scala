5bad76916901791b9283d83d336f70431ed515c7
Nathan Hamblen <nathan@technically.us>
2011-10-14 19:54:17 -0400

    barely sort of fake out serving a file

--- src/main/scala/Activity.scala
+++ src/main/scala/Activity.scala
@@ -3,7 +3,10 @@ package spur.shouty
 import _root_.android.app.Activity
 import _root_.android.os.Bundle
 import _root_.android.widget.TextView
-
+import org.jboss.netty.channel.{Channel,ChannelFuture,ChannelFutureListener}
+import org.jboss.netty.handler.codec.http.{HttpHeaders, DefaultHttpChunk,
+                                           DefaultHttpChunkTrailer}
+import org.jboss.netty.channel.group.{DefaultChannelGroup, ChannelGroupFuture}
 class MainActivity extends Activity {
   override def onCreate(savedInstanceState: Bundle) {
     super.onCreate(savedInstanceState)
@@ -26,9 +29,56 @@ class MainActivity extends Activity {
 
     import unfiltered.request._
     import unfiltered.response._
-    val hello = unfiltered.netty.cycle.Planify {
-       case _ => ResponseString("hello world")
+    import unfiltered.netty._
+    object Hello extends async.Plan with ServerErrorResponse {
+      val ChunkedMp3 =
+        unfiltered.response.Connection(HttpHeaders.Values.CLOSE) ~>
+        TransferEncoding(HttpHeaders.Values.CHUNKED) ~>
+        ContentType("audio/mpeg3")
+      val listeners = new DefaultChannelGroup
+      def intent = {
+        case req =>
+          val initial = req.underlying.defaultResponse(ChunkedMp3)
+          val ch = req.underlying.event.getChannel
+          ch.write(initial).addListener { () =>
+            listeners.add(ch)
+          }
+
+      }
+      def tick(payload: Array[Byte], len: Int) {
+        import org.jboss.netty.buffer.ChannelBuffers
+        println("tick")
+        val chunk = new DefaultHttpChunk(
+          ChannelBuffers.copiedBuffer(payload, 0, len)
+        )
+        listeners.write(chunk)
+      }
     }
-    unfiltered.netty.Http(8080).plan(hello).start()
+    unfiltered.netty.Http(8080).plan(Hello).start()
+
+    import android.os.Handler
+    val handler = new Handler
+    handler.post(new Runnable{
+      def run {
+        import java.io.{File,FileInputStream,BufferedInputStream}
+        val file = new File("/mnt/sdcard/Music/test.mp3");
+        val is = new BufferedInputStream(new FileInputStream(file))
+        val buf = new Array[Byte](1024*16)
+
+        @annotation.tailrec def read() {
+          val len = is.read(buf)
+          if (len > -1) {
+            Hello.tick(buf, len)
+            Thread.sleep(500)
+            read()
+          }
+        }
+        read()
+      }
+    })
   }
+  implicit def block2listener[T](block: () => T): ChannelFutureListener =
+    new ChannelFutureListener {
+      def operationComplete(future: ChannelFuture) { block() }
+    }
 }
